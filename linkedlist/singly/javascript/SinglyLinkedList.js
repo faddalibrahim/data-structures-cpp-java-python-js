8c9221e6a7d332c03c6c0754c636e304dfc15b81
Faddal Ibrahim <faddalibrahim@gmail.com>
2021-11-08 17:59:45 +0000

    linkedlist javascript

---
+++ linkedlist/singly/javascript/SinglyLinkedList.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class SinglyLinkedList {
+  head = null;
+  tail = null;
+
+  addLast(data) {
+    let newNode = new Node(data);
+
+    if (this.head == null) {
+      this.head = this.tail = newNode;
+      //   this.head = this.tail = new Node(data);
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      // this.tail.next = this.tail = new Node(data)
+    }
+  }
+
+  printData() {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      console.log(currentNode.data);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+const sll = new SinglyLinkedList();
+sll.addLast(12);
+sll.addLast(13);
+sll.addLast(15);
+sll.addLast(16);
+
+console.log(sll);
+sll.printData();
+
+// console.log(SinglyLinkedList.isEmpty());
