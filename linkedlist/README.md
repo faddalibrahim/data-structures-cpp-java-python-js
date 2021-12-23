1b2758f4df57929fd2a7fddd0b9a1428802ab005
Faddal Ibrahim <faddalibrahim@gmail.com>
2021-12-23 15:56:26 +0000

    linked list reversal works

--- linkedlist/README.md
+++ linkedlist/README.md
@@ -1,20 +1,21 @@
 # LinkedLists
 
-- Used to store a list of items in sequence. Unlike arrays, LinkedLists can grow and shrink automatically.  
+- Used to store a list of items in sequence. Unlike arrays, LinkedLists grow and shrink automatically.
 - Consist of a group of nodes in sequence. Each node holds two pieces of data -- value and address of the next node in the list.
-- First node : Head  
+- First node : Head
 - Last node : Tail
-
+- CS Applications: implement stack, queue, deque, adjacency list representation of graphs, dynamic memory allocation
+- SWE Applications: Image Viewers, Music Playlist, Previous and Next Pages in browsers
 
 # Runtime of Operations
 
-## LookUp : O(n)
+## LookUp
 
-- **By value**: The linkedlist must be traversed while simultaneously comparing with the value at each node till the value being searched for is matched.
+- **By value (Search)**: The linkedlist must be traversed while simultaneously comparing with the value at each node till the value being searched for is matched. **O(n)**
 
-- **By index** : Unlike arrays, linked lists nodes may not be together in memory. Thats why each node keeps a reference to the next node. Looking up an item by its index is still O(n) since the traversal has to take place.
+- **By index (Access)** : Unlike arrays, linked lists nodes may not be together in memory. Thats why each node keeps a reference to the next node. Accessing an item by its index is **O(n)** since the traversal has to be done.
 
-## Insertion : O(1) / O(n)
+## Insertion
 
 - **At the end** : Create a new node and have the tail point to it and set the tail to reference this new node **O(1)**
 
@@ -22,18 +23,15 @@
 
 - **In the middle** (say after the 10th node) : we have to traverse to the 10th node and update the links **O(n)**
 
-## Removal/Deletion : O(1) / O(n)
+## Deletion
 
 - **From the beginning**: Set the head to point to the second node, remove the link from the previous head so that it doesn't reference the second node (now the head) anymore **O(1)**
 - **From the end** : Traverse the list all the way to the penultimate item, then unlink it from the tail and finally have the tail point to the penultimate node. **O(n)**
 - **From the middle** : Traverse to the node as well as get its preceeding node. Link the preceding node to the node after our target node. Then remove the links surrounding the target node. **O(n)**
 
-
-
 # Limitations
+
 extra memory to store next node
-
-
 
 # Overcoming Limitations
 
@@ -41,18 +39,16 @@ extra memory to store next node
 
 ### Todo
 
-
-
-Operations | Array | LinkedList | Dynamic Array
-| --- | --- | --- | --- |
-| Lookup By Index | O(1) | O(n)  | O(1)
-| Lookup By value | O(n) | O(n)  | O(n)
-| Insert(begining) | O(n) | O(1) | O(n)
-| Insert (middle) | O(n) | O(n) | O(n)
-| Insert(end) | O(n) | O(1) | O(n)
-| Delete(begining) | O(n) | O(1) | O(n)
-| Delete(middle) | O(n) | O(n) | O(n)
-| Delete(end) | O(1) | O(n) , O(1) if doublylinked | O(n)
+| Operations       | Array | LinkedList                  | Dynamic Array |
+| ---------------- | ----- | --------------------------- | ------------- |
+| Lookup By Index  | O(1)  | O(n)                        | O(1)          |
+| Lookup By value  | O(n)  | O(n)                        | O(n)          |
+| Insert(begining) | O(n)  | O(1)                        | O(n)          |
+| Insert (middle)  | O(n)  | O(n)                        | O(n)          |
+| Insert(end)      | O(n)  | O(1)                        | O(n)          |
+| Delete(begining) | O(n)  | O(1)                        | O(n)          |
+| Delete(middle)   | O(n)  | O(n)                        | O(n)          |
+| Delete(end)      | O(1)  | O(n) , O(1) if doublylinked | O(n)          |
 
 With doubly-linkedlists, deleting an item from the end would be O(1). Since the last item has reference to the penultimate, there is no need to traverse sequentially to find the penultimate item.
 
--- linkedlist/python/linked_list.py
+++ linkedlist/python/linked_list.py
@@ -7,6 +7,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.__size = 0
 
     def __iter__(self):
         node = self.head
@@ -28,23 +29,30 @@ class LinkedList:
 
         return llist
 
+    def is_empty(self):
+        return self.head == None
+
     def add_first(self,value):
         new_node = Node(value)
 
         # list is empty
-        if not self.head:
+        if self.is_empty():
             self.head = self.tail = new_node
+            self.__size += 1
             return
 
         # list not empty
         new_node.next = self.head
         self.head = new_node
 
+        self.__size += 1
+
     def add_last(self,value):
         new_node = Node(value)
 
         # list is empty
-        if not self.head:
+        if self.is_empty():
+            self.__size += 1
             self.head = self.tail = new_node
             return
 
@@ -52,8 +60,10 @@ class LinkedList:
         self.tail.next = new_node
         self.tail = new_node
 
+        self.__size += 1
+
     def delete_first(self):
-        if not self.head:
+        if self.is_empty():
             raise TypeError("value is None")
 
         if self.head == self.tail:
@@ -66,30 +76,103 @@ class LinkedList:
 
     
     def delete_last(self):
-        if not self.tail:
+        if self.is_empty():
             raise TypeError("value is None")
 
         if self.head == self.tail:
             self.head = self.tail = None
             return
 
-        current = self.head
-        while current.next != self.tail:
-            current = current.next
+        penultimate = self.get_penultimate()
+        self.tail = penultimate
+        self.tail.next = None
 
-        self.tail = current
-        current.next = None
+    def get_penultimate(self):
+        penultimate = self.head
+
+        while penultimate.next != self.tail:
+            penultimate = penultimate.next
+
+        return penultimate
+
+    def index_of(self,value):
+        current_node = self.head
+        index = 0
+
+        while current_node:
+            if current_node.value == value:
+                return index
+            current_node = current_node.next
+            index += 1
+
+        return -1
+
+    def contains(self,value):
+        return self.index_of(value) != -1
+
+    def get_size(self):
+        return self.__size
+
+    def value_at(self,input_index):
+        current_node = self.head
+
+        index = 0
+        while current_node:
+            if index == input_index:
+                return current_node.value
+
+            index += 1
+            current_node = current_node.next
+        
+        raise IndexError("index out of range")
+
+    def reverse(self):
+        if self.is_empty():
+            return
+
+        previous = self.head
+        current = self.head.next
+
+        while current:
+            nextt = current.next
+
+            #reverse
+            current.next = previous
+
+            #advance pointers
+            previous = current
+            current = nextt
+
+        #reset head and tail
+        self.tail = self.head
+        self.tail.next = None
+        self.head = previous
+
+        
 
 
    
 
 ll = LinkedList()
 # print(ll.head.next)
-# ll.add_last(90)
-# ll.add_last(67)
-# ll.add_last(34)
+ll.add_last(90)
+ll.add_last(67)
+ll.add_last(34)
+ll.add_last(100)
 
-ll.add_first(12)
+print(ll)
+ll.reverse()
+print(ll)
+print(ll)
+
+print(ll.tail == ll.head)
+# print(ll.head.value)
+
+# print(ll.value_at(0))
+# ll.__size = 100
+# print(ll.__size)
+
+# ll.add_first(12)
 # ll.add_first(90)
 # ll.add_first(67)
 # ll.add_first(34)
@@ -98,6 +181,13 @@ ll.add_first(12)
 
 # print(ll)
 # ll.delete_first()
-print(ll)
-ll.delete_last()
-print(ll)
+# print(ll)
+# ll.delete_first()
+# ll.delete_first()
+# ll.delete_first()
+# ll.delete_last()
+# ll.delete_last()
+# ll.delete_last()
+# ll.delete_last()
+# ll.delete_last()
+# print(ll)
