65188a73e0798554c3f51f655fe18f38c22c335e
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-01-03 23:23:02 +0000

    tree max path sum

---
+++ tree/python/max_path_sum.py
@@ -0,0 +1,8 @@
+def max_path_sum(root):
+    if not root:
+        return float('-inf')
+
+    if not root.left and not root.right:
+        return root.val
+    
+    return root.val + max(max_path_sum(root.left),max_path_sum(root.right))
